6565b568e6986e8ff29aff73e3067be29c8fd689
kongminga1981 <158770452+kongminga1981@users.noreply.github.com>
2025-09-29 10:55:04 +0800

    add open sans font, captialize title, white background

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,10 +4,10 @@ import './menu-item.style.scss'
 const MenuItem = (props) => {
     console.log(props)
     return <div
-        div style={{backgroundImage : `url(${props.imageUrl})`}}
+        style={{backgroundImage : `url(${props.imageUrl})`}}
         className= {`${props.size} menu-item`} >
         <div className='content'>
-            <h1 className='title'>{props.title}</h1>
+            <h1 className='title'>{props.title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
